87a3652d5a11cceff4a0aa909781893d0b61dc32
Sviatoslav Smovdyr <48630769+SmSv90@users.noreply.github.com>
2020-10-24 13:05:12 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -39,6 +39,36 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let words = [],
+        morseStr = '',
+        messageConverted = [];
+
+    for (let i = 0; i < expr.length-1; i+=10) {
+        words.push(expr.substring(i, i+10));
+    }
+
+    for (let elem of words) {
+        for (let j = 1; j < elem.length; j+=2 ) {
+            if (elem[j-1] == 1 && elem[j] == 0) {
+                morseStr += '.';
+            } else if (elem[j-1] == 1 && elem[j] == 1) {
+                morseStr += '-';
+            } else if (elem[j-1] == "*" && elem[j] == "*") {
+                morseStr += ' ';
+            } 
+        }
+        morseStr += '#';
+    }
+   
+    morseStr.split("     ").map(function (word) {
+        word.split("#").map(function (letter) {
+            messageConverted.push(MORSE_TABLE[letter]);
+        });
+        messageConverted.push(" ");
+    });
+    messageConverted = messageConverted.slice(0, -1);
+    return messageConverted.join("");
+
 }
 
 module.exports = {
